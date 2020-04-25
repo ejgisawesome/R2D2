c6d5922261c2b1293b0531960e0d6933c25eb133
ejgisawesome <egarcia4441@gmail.com>
2020-04-24 21:54:54 -0400

    Update the spice melange.R

--- the spice melange.R
+++ the spice melange.R
@@ -239,3 +239,12 @@ x$f
 x[["f"]]
 x[["f",exact=FALSE]]
 
+#Removing Missing Values (Multiple)
+x <- c(1,2,NA,4,NA,5)
+bad <- is.na(x)
+x[!bad]
+y <- c("a","b",NA,"d",NA,"f")
+good <- complete.cases(x,y) #beast function
+good
+
+#Vectorized Operations

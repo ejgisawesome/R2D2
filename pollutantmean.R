cff4fce3da22dcb746b26cc94bbb06136db4412b
ejgisawesome <egarcia4441@gmail.com>
2020-05-01 02:17:44 -0400

    good try old sport

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,17 +1,27 @@
 #Pollutant Mean Formula
+directory <- '~/specdata'
+directory <- 'specdata'
+
 
 pollutantmean <- function(directory,pollutant,id = 1:332) {
         ## 'directory' is a character vector of length 1 indicating
         ## the location of CSV files
         
-        directory <- '~/specdata'
-        setwd(directory)
+        direct <- paste('~/',directory,sep="")
+        setwd(direct)
+        getwd()
+        files <- dir(direct)
+        
+        alldata <- do.call(rbind,lapply(files,read.csv))
+        x <- id
+        somedata <- do.call(rbind,lapply(files[x],read.csv))
         
         
         ## 'pollutant' is a character vector of length 1 indicating the
         ## name of the pollutant for which we will calculate the mean;
         ## either 'sulfate' or 'nitrate'
         
+        pollutant <- names(data)
         
         
         ## 'id' is an integer vector indicating the monitor ID numbers
@@ -20,7 +30,8 @@ pollutantmean <- function(directory,pollutant,id = 1:332) {
         ## Return the mean of the pollutant across all monitors list
         ## in the 'id' vector (ignoring NA values)
         ## NOTE: Do not round the result!
-
+        useful <- mean(data$sulfate,na.rm = TRUE)
+         
 }
 
 ?id
--- pollutantmeanstest.R
+++ pollutantmeanstest.R
@@ -1,14 +1,29 @@
+rm(list=ls())
 #home
 directory <- '~/specdata'
 
 #this didn't work
-install.packages('devtools')
-library(devtools)
-wd(directory)
+#install.packages('devtools')
+#library(devtools)
+#wd(directory)
 
 #set the directory properly this way
 ?setwd
+directory <- '~/specdata'
 setwd(directory)
+#to go back
 setwd('~/GitHub/R2D2')
 getwd()
+#fuck around and get it
 read.csv('001.csv')
+x <- 1:101
+file <- c(paste('0','0',x[1:9],'.csv',sep=""),paste('0',x[10:99],'.csv',sep=""),paste(x[100:length(x)],'.csv',sep=""))
+file
+check <- read.csv(file[1:101])
+check$sulfate
+file
+
+?lapply
+
+source("pollutantmean.R")
+pollutantmean("specdata","sulfate",1:10)

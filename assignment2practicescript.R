7ddc1c42fe632ac89d633f4ae1004a8b4da2d9ac
ejgisawesome <egarcia4441@gmail.com>
2020-05-13 20:18:24 -0400

    sundown

---
+++ assignment2practicescript.R
@@ -0,0 +1,48 @@
+## PRACTICE FILE
+rm(list=ls())
+## A place to try out functions and possible solutions
+install.packages("matlib")
+library(matlib)
+?inv
+
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
+}
+
+library(datasets)
+
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) m <<- inv
+        getinv <- function() m
+        list(set = set,get = get,
+             setinv = setinv,
+             getinv = getinv)
+}

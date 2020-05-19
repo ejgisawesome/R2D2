7dfe170738e69c63bd864a49be558314b45a7412
ejgisawesome <egarcia4441@gmail.com>
2020-05-18 21:47:22 -0400

    Create swirls to unfurl.R
    Looking at Data
    Simulation
    Base Graphics

---
+++ swirls to unfurl.R
@@ -0,0 +1,23 @@
+## WEEK 4 QUIZ
+rm(list=ls())
+set.seed(1)
+rpois(5,2)
+
+
+set.seed(10)
+x <- rep(0:1, each = 5)
+e <- rnorm(10, 0, 20)
+y <- 0.5 + 2 * x + e
+
+## 90%
+
+swirl()
+library(swirl)
+
+## YOUR SWIRLS ARE...
+'
+Looking at Data
+Simulation
+Base Graphics
+'
+

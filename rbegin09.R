87c0798cd38bada7adbd232724196202fef4b6e5
ejgisawesome <egarcia4441@gmail.com>
2020-05-29 21:52:21 -0400

    Create rbegin09.R
    freemium isn't free

---
+++ rbegin09.R
@@ -0,0 +1,16 @@
+rm(list=ls())
+
+## 2009 A Beginner's Guide To R
+
+## suggestions:
+
+## If you don't know shit
+## Chapters 1,2,3,5
+
+## If you know your shit
+## Chapters 6,8,9
+
+
+
+## CHAPTER 2 GETTING DATA INTO R
+

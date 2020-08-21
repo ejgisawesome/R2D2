46665fd09dfb50a5a0663812a93037aa1c6755ed
ejgisawesome <egarcia4441@gmail.com>
2020-08-20 22:04:38 -0400

    why is vs code so shit

---
+++ sheet_weight.R
@@ -0,0 +1,8 @@
+
+
+#density list
+
+#STEEL MILD
+den_st <- 7.850 * 0.0361273
+# [1] 0.2835993
+
---
+++ sqft.R
@@ -0,0 +1,4 @@
+sqft <- function (a,b,c=12){
+        area <- (a/12)*(b/12)*(c/12)
+        return(area)
+}

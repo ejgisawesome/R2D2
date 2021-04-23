6e142a58d50a012855280c2b71c0354178c8ecf1
ejgisawesome <egarcia4441@gmail.com>
2021-04-23 15:55:15 -0400

    Update stonks.R

--- stonks.R
+++ stonks.R
@@ -9,7 +9,7 @@ install.packages("tidyquant")
 #LOAD PACKAGE
 library(quantmod)
 
-bal <- 9.91
+bal <- 2.48
 shares <- function(stonk,deposit=0,balance=bal){
         num <- floor((deposit + balance)/price(stonk))
         return(num)
